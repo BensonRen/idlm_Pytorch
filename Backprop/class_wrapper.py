Small bug fix for Backprop hyperswipe

Backprop/class_wrapper.py
@@ -77,9 +77,10 @@ class Network(object):
         if logit is None:
             return None
         MSE_loss = nn.functional.mse_loss(logit, labels)          # The MSE Loss
-
-        # Boundary loss of the geometry_eval to be less than 1
-        BDY_loss = torch.mean(torch.clamp(torch.abs(self.model.geometry_eval) - 1, min=0, max=inf))
+        BDY_loss = 0
+        if self.model.bp:
+            # Boundary loss of the geometry_eval to be less than 1
+            BDY_loss = torch.mean(torch.clamp(torch.abs(self.model.geometry_eval) - 1, min=0, max=inf))
         self.MSE_loss = MSE_loss
         self.Boundary_loss = BDY_loss
         return torch.add(MSE_loss, BDY_loss)

Backprop/model_maker.py
@@ -58,7 +58,7 @@ class Backprop(nn.Module):
         """
         out = G                                                         # initialize the out
         if self.bp:                                               # If the evaluation mode
-            out = self.geometry_eval
+           out = self.geometry_eval
         # For the linear part
         for ind, (fc, bn) in enumerate(zip(self.linears, self.bn_linears)):
             # print(out.size())