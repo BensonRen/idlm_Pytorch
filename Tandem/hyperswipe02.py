2e99fdfe4aad83b4b11cb1875be4fecdbd1fed82
Ben <sr365@duke.edu>
2020-01-27 14:11:36 -0500

    plotswipe naming changed

--- Tandem/hyperswipe02.py
+++ Tandem/hyperswipe02.py
@@ -6,7 +6,7 @@ import train
 import  numpy as np
 import flag_reader
 if __name__ == '__main__':
-    linear_unit_list = [150, 300]
+    linear_unit_list = [5, 10, 15, 20, 25]
     #linear_unit_list = [1000, 500]
     #linear_unit_list = [1000, 500, 300, 150]
     # reg_scale_list = [1e-5, 5e-5, 1e-4, 5e-4, 1e-3, 5e-3]
@@ -16,12 +16,12 @@ if __name__ == '__main__':
         for i in range(4, 10):
             flags = flag_reader.read_flag()  	#setting the base case
             linear = [linear_unit for j in range(i)]        #Set the linear units
-            linear[0] = 150                   # The start of linear
-            linear[-1] = 8                # The end of linear
+            linear[0] =  1                   # The start of linear
+            linear[-1] = 2                # The end of linear
             flags.linear_b = linear
             for reg_scale in reg_scale_list:
                 flags.reg_scale = reg_scale
                 for j in range(3):
-                        flags.model_name = "reg"+ str(flags.reg_scale) + "trail_"+str(j)+"_tandem_linear_b_complexity_swipe_layer" + str(linear_unit) + "_num" + str(i)
+                        flags.model_name = flags.data_set + "reg"+ str(flags.reg_scale) + "trail_"+str(j)+"_tandem_linear_b_complexity_swipe_layer" + str(linear_unit) + "_num" + str(i)
                         train.training_from_flag(flags)
 
--- Tandem/hyperswipe04.py
+++ Tandem/hyperswipe04.py
@@ -6,21 +6,22 @@ import train
 import  numpy as np
 import flag_reader
 if __name__ == '__main__':
-    #linear_unit_list = [150, 300]
-    linear_unit_list = [500, 1000]
+    linear_unit_list = [5, 10, 15, 20, 25]
+    #linear_unit_list = [1000, 500]
     #linear_unit_list = [1000, 500, 300, 150]
+    # reg_scale_list = [1e-5, 5e-5, 1e-4, 5e-4, 1e-3, 5e-3]
     reg_scale_list = [5e-3]
     for linear_unit in linear_unit_list:
         # Setting the loop for setting the parameter
-        for i in range(7,10):
+        for i in range(4, 10):
             flags = flag_reader.read_flag()  	#setting the base case
             linear = [linear_unit for j in range(i)]        #Set the linear units
-            linear[0] = 150                   # The start of linear
-            linear[-1] = 8                # The end of linear
+            linear[0] =  1                   # The start of linear
+            linear[-1] = 2                # The end of linear
             flags.linear_b = linear
             for reg_scale in reg_scale_list:
                 flags.reg_scale = reg_scale
                 for j in range(3):
-                        flags.model_name = "reg"+ str(flags.reg_scale) + "trail_"+str(j)+"_tandem_linearb_complexity_swipe_layer" + str(linear_unit) + "_num" + str(i)
+                        flags.model_name = flags.data_set + "reg"+ str(flags.reg_scale) + "trail_"+str(j)+"_tandem_linear_b_complexity_swipe_layer" + str(linear_unit) + "_num" + str(i)
                         train.training_from_flag(flags)
 
