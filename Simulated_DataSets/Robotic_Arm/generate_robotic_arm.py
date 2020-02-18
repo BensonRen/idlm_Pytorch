0ccf3bba75a6e05195823a8ef43afc1bf50b9023
Ben <rensimiao.ben@gmail.com>
2020-02-18 10:33:34 -0500

    Rotbotic arm program fixed to be the same distribution as the original paper

--- Simulated_DataSets/Robotic_Arm/generate_robotic_arm.py
+++ Simulated_DataSets/Robotic_Arm/generate_robotic_arm.py
@@ -11,15 +11,11 @@ from os.path import join
 from numpy import sin, cos
 
 # Define some hyper-parameters
-origin_limit = 3                                                    # The max distance the origin can move to in y axis
 arm_num = 3                                                         # Number of arms
-arm_lengths = [1, 1, 2]                                             # length of the robotic arm
-arm_max_length = np.sum(arm_lengths) + origin_limit                 # Drawing limit
-num_samples_per_dimension = 2                                       # Number of samples drawn from each dof
-angle_sample_range = np.array([-0.5*pi, 0.5*pi])                    # Specify the angle sample range
-# angle_sample_range = np.array([0, 0])                    # Specify the angle sample range
-sample_ranges = [angle_sample_range for i in range(arm_num + 1)]    # List the range of samples to take, for angles
-sample_ranges[0] = [-origin_limit, origin_limit]                    # Modify the first to be origin y axis
+arm_lengths = [0.5, 0.5, 1]                                         # length of the robotic arm
+arm_max_length = np.sum(arm_lengths)                                # Drawing limit
+num_samples = 10000
+sample_vars = [1/16, 1/4, 1/4, 1/4]
 eps = 0.2
 
 def determine_final_position(origin_pos, arm_angles, arm_lengths=arm_lengths, evaluate_mode=False):
@@ -34,18 +30,6 @@ def determine_final_position(origin_pos, arm_angles, arm_lengths=arm_lengths, ev
     :return: positions: list of #arm_num of [N, 2], The positions that the arms nodes for plotting purpose
     """
     # First make sure that the angles are legal angles
-    #print("shape of arm_angles", np.shape(arm_angles))
-    #print("shape of original_pos", np.shape(origin_pos))
-    #print(np.max(arm_angles))
-    for i in range(arm_num):
-        assert (np.max(arm_angles[:, i]) < pi) and (np.min(arm_angles[:, i]) > -pi), \
-            'Your angle has to be within [-pi, pi]'
-    # This is for normalized data for inference!!!
-    if evaluate_mode:
-        origin_pos *= origin_limit
-        arm_angles *= 0.5 * pi
-    else:
-        print("max mins are", np.max(origin_pos), np.min(origin_pos), np.max(arm_angles), np.min(arm_angles))
     # Start computing for positions
     positions = []                                          # Holder for positions to be plotted
     current_pos = np.zeros([len(origin_pos), 2])            # The robotic arm starts from [0, y]
@@ -53,15 +37,18 @@ def determine_final_position(origin_pos, arm_angles, arm_lengths=arm_lengths, ev
     current_pos[:, 0] = 0                                   # The x axis is 0 for all cases
     current_pos[:, 1] = origin_pos                          # Plug in the
     positions.append(np.copy(current_pos))
+
+    # Set up the arm angle difference matrix for compliance to the original paper
+    arm_angles_diff = np.copy(arm_angles)
+    arm_angles_diff[:, 1] -=  arm_angles[:, 0]
+    arm_angles_diff[:, 2] -=  arm_angles[:, 1] + arm_angles[:, 0]
+
     for arm_index in range(arm_num):                                # Loop through the arms
         # print("i=",i)
-        current_pos[:, 0] += cos(arm_angles[:, arm_index]) * arm_lengths[arm_index]
-        current_pos[:, 1] += sin(arm_angles[:, arm_index]) * arm_lengths[arm_index]
+        current_pos[:, 0] += cos(arm_angles_diff[:, arm_index]) * arm_lengths[arm_index]
+        current_pos[:, 1] += sin(arm_angles_diff[:, arm_index]) * arm_lengths[arm_index]
         positions.append(np.copy(current_pos))
     plot_arms(np.array(positions))
-    if evaluate_mode:                               # IF the data needs normalization
-        current_pos[:, 0] = (current_pos[:, 0] - 2) /2.
-        current_pos[:, 1] = current_pos[:, 1] / 7
     return current_pos, np.array(positions)
 
 
@@ -77,44 +64,53 @@ def plot_arms(positions, save_dir='.', save_name='robotic_arms_plot.png', margin
     f = plt.figure()
     shape = np.shape(positions)
     # Draw the verticle line for the origin arm
-    plt.plot([0, 0], [-arm_max_length, arm_max_length], lw=1, c='b')
-    #plt.plot(positions[1:, :, 0], positions[1:, :, 1], ms=5, mfc='w')#, mec='b')
-    plt.plot(positions[:, 0, 0], positions[:, 0, 1], lw=3, ls='-', c='k', alpha=0.1)
+    plt.plot([0, 0], [-1.5, 1.5], lw=1, c='b')
+    # plt.plot(positions[1:, :, 0], positions[1:, :, 1], ms=5, mfc='w')#, mec='b')
+    # plt.plot(positions[:, 0, 0], positions[:, 0, 1], lw=3, ls='-', c='k', alpha=0.1)
+    print("shape of positions", np.shape(positions))
+    # print(np.min(positions[:, :, 0], axis=0) )
+    strange_list = np.arange(shape[1])[np.min(positions[:, :, 0], axis=0) < 0]
+    print("strange list is", strange_list)
+    print("position of those strange ones are", positions[:, strange_list, :])
+    # print(positions)
     for i in range(shape[1]):
         #print("i =",i)
         #print("position0:", positions[:, i, 0])
         #print("position1:", positions[:, i, 1])
-        plt.plot(positions[:, i, 0], positions[:, i, 1], lw=2, ls='-', c='k', alpha=1)
-        plt.scatter(positions[:, i, 0], positions[:, i, 1], c='b', s=30, marker='o')
-        plt.scatter(positions[:, i, 0], positions[:, i, 1], c='w', s=20, marker='o')
-    #plt.ylim([-arm_max_length, arm_max_length])
-    #plt.xlim([-margin, arm_max_length - origin_limit + margin])
+        plt.plot(positions[:, i, 0], positions[:, i, 1], lw=1, ls='-', c='k', alpha=0.1)
+        if i in strange_list:
+            print(i)
+            plt.plot(positions[:, i, 0], positions[:, i, 1], lw=2, c='b', alpha=0.5)
+        #plt.scatter(positions[:, i, 0], positions[:, i, 1], c='b', s=30, marker='o')
+        #plt.scatter(positions[:, i, 0], positions[:, i, 1], c='w', s=20, marker='o')
+
+    plt.plot()
+    # plt.ylim([-arm_max_length, arm_max_length])
+    # plt.xlim([-margin, arm_max_length - origin_limit + margin])
     f .savefig(join(save_dir, save_name))
 
 
 def Sample_through_space():
-    samples = []
-    for sample_range in sample_ranges:
-        samples.append(np.linspace(sample_range[0], sample_range[1], num=num_samples_per_dimension))
-    x = np.array(np.meshgrid(*samples))
-    print("shape of x is: ", np.shape(x))
-    data_x = np.concatenate([np.reshape(np.ravel(x[j, :]), [-1, 1]) for j in range(arm_num + 1)], axis=1)
-    #print("data_x", data_x)
+    # Initialize the data
+    data_x = np.random.randn(num_samples, arm_num + 1)
+    for i in range(len(data_x[0, :])):
+        data_x[:, i] *= np.sqrt(sample_vars[i])
+        print(np.var(data_x[:, i]))
+    # print("data_x", data_x)
     print("shape of data_x is:", np.shape(data_x))
     data_y, positions = determine_final_position(data_x[:, 0], data_x[:, 1:])
-    #print("data_y", data_y)
+    # print("data_y", data_y)
     print("shape of data_y is", np.shape(data_y))
     print("shape of positions is:", np.shape(positions))
-    #print("position=", positions)
+    # print("position=", positions)
     plot_arms(positions)
-
     np.savetxt('data_x.csv', data_x, delimiter=',')
     np.savetxt('data_y.csv', data_y, delimiter=',')
 
 
 if __name__ == '__main__':
-    positions = np.array([np.reshape(np.array([0, 2]), [-1, 2]), np.reshape(np.array([1, 4]), [-1, 2]),
-                          np.reshape(np.array([2, 3]), [-1, 2]), np.reshape(np.array([3, 0.2]), [-1, 2])])
-    print("original position shape", np.shape(positions))
-    plot_arms(positions)
-    # Sample_through_space()
+    # positions = np.array([np.reshape(np.array([0, 2]), [-1, 2]), np.reshape(np.array([1, 4]), [-1, 2]),
+    #                          np.reshape(np.array([2, 3]), [-1, 2]), np.reshape(np.array([3, 0.2]), [-1, 2])])
+    # print("original position shape", np.shape(positions))
+    # plot_arms(positions)
+    Sample_through_space()
