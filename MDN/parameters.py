3e40a646922cbd8d7f8cba55c14774774bdf6336
Ben <sr365@duke.edu>
2020-09-11 14:49:25 -0400

    MDN added

---
+++ MDN/parameters.py
@@ -0,0 +1,41 @@
+"""
+Params for Back propagation model
+"""
+# Define which data set you are using
+DATA_SET = 'meta_material'
+# DATA_SET = 'gaussian_mixture'
+# DATA_SET = 'sine_wave'
+# DATA_SET = 'naval_propulsion'
+# DATA_SET = 'robotic_arm'
+
+# Model Architectural Params for meta_material data Set
+NUM_GAUSSIAN = 3
+LINEAR = [300,  1000, 1000, 1000, 1000, 8]
+
+# Optimizer Params
+OPTIM = "Adam"
+REG_SCALE = 1e-3
+BATCH_SIZE = 128
+EVAL_BATCH_SIZE = 4096
+EVAL_STEP = 50
+TRAIN_STEP = 300
+BACKPROP_STEP = 200
+LEARN_RATE = 1e-4
+# DECAY_STEP = 25000 # This is for step decay, however we are using dynamic decaying
+LR_DECAY_RATE = 0.5
+STOP_THRESHOLD = 1e-4
+
+# Data specific Params
+X_RANGE = [i for i in range(2, 10 )]
+Y_RANGE = [i for i in range(10 , 2011 )]
+FORCE_RUN = True
+MODEL_NAME = None
+#DATA_DIR = '../'
+#DATA_DIR = '/work/sr365/'
+DATA_DIR = 'D:\AML\idlm_Ben'
+GEOBOUNDARY =[30, 52, 42, 52]
+NORMALIZE_INPUT = True
+
+# Running specific
+USE_CPU_ONLY = False
+EVAL_MODEL = None
--- run02.sh
+++ run02.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --output output02.err                                                   # output log file
 #SBATCH -e error02.err                                                   # error log file
-#SBATCH --mem=20G                                                      # request 20G memory
+#SBATCH --mem=40G                                                      # request 40G memory
 #SBATCH -c 1                                                           # request 6 gpu cores                                    
 #SBATCH -p collinslab                                     # request 1 gpu for this job
 #SBATCH --exclude=dcc-collinslab-gpu-[01,03,04]
@@ -10,6 +10,14 @@ module load Anaconda3/3.5.2                                            # load co
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/opt/apps/rhel7/cudnn/lib64:$LD_LIBRARY_PATH
 # add my library path
-export PYTHONPATH=$PYTHONPATH:/hpc/home/sr365/Pytorch
+export PYTHONPATH=$PYTHONPATH:/hpc/home/sr365/NIPS/idlm_Pytorch/
 # execute my file
-python hyperswipe02.py
+# python hyperswipe02.py
+# python utils/get_mask.py
+# python utils/train_test_split.py
+# python plotswipe.py
+#python -m cProfile train.py
+# python train.py
+# python test.py
+#python evaluate.py
+python generate_robotic_arm.py
